29dc4fd4dea1953b68259fea8c4072be6c18d7d8
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-09-13 13:18:09 -0500

    First Name, Last Name, Age

---
+++ src/outputExamples/FirstNameLastNameAge.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class FirstNameLastNameAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Aafreen"); System.out.println("Sajid"); System.out.println("age=17");
+
+	}
+
+}
