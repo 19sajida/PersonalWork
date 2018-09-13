641ba10fb0b5b21da4a6eede078d4664fcd6663e
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-09-13 13:49:53 -0500

    Mailing Label

---
+++ src/outputExamples/MailingLabel.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class MailingLabel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" Aafreen Sajid \n 18950 Ashford Ln \n Brookfield, WI 53045");
+	}
+
+}
