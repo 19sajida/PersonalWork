14e8a71acbb31a2ed73ade11442815841bda4b9a
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-09-13 14:04:35 -0500

    Initials

---
+++ src/outputExamples/Initials.java
@@ -0,0 +1,14 @@
+package outputExamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("     A       SSSSS");
+System.out.println("    A A     S");
+System.out.println("   AAAAA     SSSSS");
+System.out.println("  A     A         S");
+System.out.println(" A       A   SSSSS");
+	}
+
+}
