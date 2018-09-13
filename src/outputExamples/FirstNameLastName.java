7ec21b1e688dfd28f39b4cd25d60b72f848031b2
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-09-13 13:10:33 -0500

    First Name, Last Name

---
+++ src/outputExamples/FirstNameLastName.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class FirstNameLastName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Aafreen"); System.out.println("Sajid");
+	}
+
+}
