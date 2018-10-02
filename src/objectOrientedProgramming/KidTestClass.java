01bb8434d1771a545ab81baed9985468f66153e8
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-10-02 14:07:05 -0500

    Kid

---
+++ src/objectOrientedProgramming/KidTestClass.java
@@ -0,0 +1,14 @@
+package objectOrientedProgramming;
+
+public class KidTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Kid sabrina = new Kid();
+sabrina.sayMama();
+sabrina.sayFood();
+sabrina.sayStink();
+
+	}
+
+}
