cdf086f6b704ac37f60c2e91626619972f642bb2
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-10-02 14:13:26 -0500

    Neighbor

---
+++ src/objectOrientedProgramming/Neighbor.java
@@ -0,0 +1,17 @@
+package objectOrientedProgramming;
+
+public class Neighbor {
+public Neighbor()
+{}
+	public void sayJoke()
+	{System.out.println("Your mom.");
+	}
+	public void sayMoney()
+	{System.out.println("Someone wants your money.");
+	}
+	public void sayConversation()
+	{System.out.println("friend 1: hi");
+	System.out.println("friend 2: shut up");System.out.println("friend 1: (is sad)");
+	
+}
+}
---
+++ src/objectOrientedProgramming/NeighborTestClass.java
@@ -0,0 +1,13 @@
+package objectOrientedProgramming;
+
+public class NeighborTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Neighbor yourmom = new Neighbor();
+yourmom.sayJoke();
+yourmom.sayMoney();
+yourmom.sayConversation();
+	}
+
+}
