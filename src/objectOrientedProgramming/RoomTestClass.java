cdea4472de96b89b70e1f03f3c245a9429094d23
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-10-03 14:38:10 -0500

    Room

---
+++ src/objectOrientedProgramming/RoomTestClass.java
@@ -0,0 +1,13 @@
+package objectOrientedProgramming;
+
+public class RoomTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Room ugh = new Room();
+ugh.area();
+ugh.areaWithDoor();
+
+	}
+
+}
