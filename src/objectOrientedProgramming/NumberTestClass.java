5f35a58d8e159f0425f8afce592208b2042e1e6a
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-10-04 14:10:38 -0500

    Numbers

---
+++ src/objectOrientedProgramming/NumberTestClass.java
@@ -0,0 +1,14 @@
+package objectOrientedProgramming;
+
+public class NumberTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Numbers bob = new Numbers();
+bob.sayNumber(7.5);
+bob.sayNumberPlus2(7.5);
+bob.saySum(1.2,1.8);
+	}
+
+}
