9b59f12f855bae443277800d8f0b9807359d66a3
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-10-05 15:04:16 -0500

    Round and Square

--- src/objectOrientedProgramming/NumberTestClass.java
+++ src/objectOrientedProgramming/NumberTestClass.java
@@ -5,10 +5,16 @@ public class NumberTestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-Numbers bob = new Numbers();
-bob.sayNumber(7.5);
-bob.sayNumberPlus2(7.5);
-bob.saySum(1.2,1.8);
+		Numbers bob = new Numbers();
+		bob.sayNumber(7.5);
+		bob.sayNumberPlus2(7.5);
+		bob.saySum(1.2, 1.8);
+		double answer = bob.returnSquare(9.0);
+		System.out.println("Your number is " + answer);
+		double area = bob.returnArea(4.0, 6.0);
+		System.out.println("Area: " + area);
+		int round = bob.returnRoundUp(7.8);
+		System.out.println("Your rounded up number is " + round);
 	}
 
 }
