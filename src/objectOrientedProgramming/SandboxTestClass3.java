9620b543421893534218d1560ec13fd07df9ed35
Unknown <aafreen.sajid@gmail.com>
2018-10-09 13:13:20 -0500

    Sandbox

---
+++ src/objectOrientedProgramming/SandboxTestClass3.java
@@ -0,0 +1,24 @@
+package objectOrientedProgramming;
+
+import java.util.Scanner;
+
+public class SandboxTestClass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is the width of the sandbox in feet?");
+		double width = input.nextDouble();
+		System.out.println("What is the length of the sandbox in feet?");
+		double length = input.nextDouble();
+		System.out.println("What is the depth of the sandbox in feet?");
+		double depth = input.nextDouble();
+		System.out.println("How many cubic feet of sand can you transport per hour?");
+		double transport = input.nextDouble();
+		Sandbox2And3 bob = new Sandbox2And3();
+		bob.Sandbox3(length, width, depth, transport);
+		double time = bob.Sandbox3(length, width, depth, transport);
+		System.out.println("It will take " + time + " hours to fill up the sand box");
+	}
+
+}
