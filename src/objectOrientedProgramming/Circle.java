cbd1cc1bdd00a90f75cc27b754b679be2cc7bf1f
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-10-10 15:00:07 -0500

    Circle Fix

--- src/objectOrientedProgramming/Circle.java
+++ src/objectOrientedProgramming/Circle.java
@@ -2,7 +2,8 @@ package objectOrientedProgramming;
 
 public class Circle {
 private double radius;
-public Circle() {}
+public Circle() {radius = 0.0;}
+public Circle(double radius2) {radius = radius2;}
 public double returnRadius() {return radius;}
 public double circumference() {double circumference = radius*2*Math.PI; return circumference;}
 public double area() {double area = radius*radius*Math.PI; return area;}
