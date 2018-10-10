fc0bc1a5a0a49c886b399c888d2a8bb944359147
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-10-10 15:00:14 -0500

    Rectangle Fix

--- src/objectOrientedProgramming/Rectangle.java
+++ src/objectOrientedProgramming/Rectangle.java
@@ -1,14 +1,43 @@
 package objectOrientedProgramming;
 
 public class Rectangle {
-private double length;
-private double width;
-public Rectangle() {length = 0.0; width = 0.0;}
-public double returnLength() {return length;}
-public double returnWidth() {return width;}
-public double perimeter() {double perimeter = 2*length+2*width; return perimeter;}
-public double area() {double area = length*width; return area;}
-public void changeLength(double newLength) {length = newLength;}
-public void changeWidth(double newWidth) {width = newWidth;}
+	private double length;
+	private double width;
+
+	public Rectangle() {
+		length = 0.0;
+		width = 0.0;
+	}
+
+	public Rectangle(double length2, double width2) {
+		length = length2;
+		width = width2;
+	}
+
+	public double returnLength() {
+		return length;
+	}
+
+	public double returnWidth() {
+		return width;
+	}
+
+	public double perimeter() {
+		double perimeter = 2 * length + 2 * width;
+		return perimeter;
+	}
+
+	public double area() {
+		double area = length * width;
+		return area;
+	}
+
+	public void changeLength(double newLength) {
+		length = newLength;
+	}
+
+	public void changeWidth(double newWidth) {
+		width = newWidth;
+	}
 
 }
