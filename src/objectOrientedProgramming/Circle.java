9e9938da5bc47834cec82864d55b5d6864703224
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-10-12 13:23:32 -0500

    Shapes

--- src/objectOrientedProgramming/Circle.java
+++ src/objectOrientedProgramming/Circle.java
@@ -3,6 +3,7 @@ package objectOrientedProgramming;
 public class Circle {
 private double radius;
 public Circle() {radius = 0.0;}
+public Circle(double radius1) {radius = radius1;}
 public double returnRadius() {return radius;}
 public double circumference() {double circumference = radius*2*Math.PI; return circumference;}
 public double area() {double area = radius*radius*Math.PI; return area;}
