2b555dbc956182289c7c1c9dea117f55a415cf7a
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-10-24 15:07:52 -0500

    Do Loop

---
+++ src/decisions/DoLoop.java
@@ -0,0 +1,34 @@
+package decisions;
+
+import java.util.Scanner;
+
+public class DoLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		/*
+		 * System.out.println("Integer:"); int num = input.nextInt(); do {
+		 * System.out.println("magic product is:" + num * 17);
+		 * System.out.println("Integer:"); num = input.nextInt(); } while (num
+		 * != 9999);
+		 */
+		System.out.println("Integer:");
+		int num = input.nextInt();
+		int largest = 0;
+		int smallest = 10000;
+		do {
+			if (largest < num) {
+				largest = num;
+			}
+			if (smallest > num) {
+				smallest = num;
+			}
+			System.out.println("Integer:");
+			num = input.nextInt();
+		} while (num != 9999);
+		int range = largest - smallest;
+		System.out.println("The range is:" + range);
+	}
+
+}
