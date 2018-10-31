6808ec47d0848c043452f2daca434609ef8d1fec
Aafreen Sajid <aafreen.sajid@gmail.com>
2018-10-31 15:05:55 -0500

    Nested Loops More

--- src/decisions/NestedLoops.java
+++ src/decisions/NestedLoops.java
@@ -20,7 +20,7 @@ public class NestedLoops {
 		 * System.out.print(counter + "\t"); }
 		 * 
 		 * } System.out.println(); }
-		 */
+		 
 
 		int counter = 0;
 		int counter2 = 0;
@@ -40,6 +40,42 @@ public class NestedLoops {
 			counter3 = 0;
 		}
 		System.out.println(number);
+		
+	
+		
+		int counter = 0;
+		int counter2 = 0;
+		int greatest = 0;
+		int counter3 = 0;
+		int number =0;
+		int enter=0;
+		System.out.println("Enter positive integer: ");
+		enter = input.nextInt();
+		for (counter = 1; counter <= enter; counter++) {
+			for (counter2 = 1; counter2 <= enter; counter2++) {
+				if (counter % counter2 == 0) {
+					counter3++;
+				}
+				if (counter3 > greatest) {
+					greatest = counter3; number=counter; 
+				}
+				
+			}
+			counter3 = 0;
+		}
+		System.out.println(number);
+		*/
+		
+		int counter = 0;
+		int counter2 =0;
+		int number =0;
+		for (counter = 1; counter <=5; counter++){
+			for (counter2 = 1; counter2 <=6; counter2=counter2+1){
+				System.out.print(counter2 + "\t");
+			}
+			number = 0;
+			System.out.println();
+		}
 	}
 
 }
